d62088073b50b03acd2fa405f7f6ba118b667c69
maketheswitch <azakani91@gmail.com>
2022-02-21 16:23:57 +0100

    Ajout des .jsp + début servlet Connexion
    Co-Authored-By: Iranja <77077635+Iranja@users.noreply.github.com>

--- src/main/java/fr/uparis/services/Connexion.java
+++ src/main/java/fr/uparis/services/Connexion.java
@@ -1,5 +1,7 @@
 package fr.uparis.services;
 
+import mediatek2022.Mediatheque;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,9 +9,20 @@ import java.io.IOException;
 
 @WebServlet(name = "Connexion", value = "/Connexion")
 public class Connexion extends HttpServlet {
+    Mediatheque m;
+
+    Connexion(){
+        /*try{
+            Class.forName(persistance.MediathequeData.class.getName());
+        }catch(Exception e){
+
+        }*/
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        RequestDispatcher rd = request.getRequestDispatcher("view/connexion.jsp");
+        response.sendRedirect("view/index.jsp");
+        rd.forward(request,response);
     }
 
     @Override
